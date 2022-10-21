10c6093375a9c680ac646f5d9b3b490b3dd23ed2
Markus Heine <markusheine.dev@gmail.com>
2022-10-21 16:14:19 +0200

    add new settings tab

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,37 @@
 import waitForElm from './helper/waitForElement';
 
-(async () => {
-    const filter = await waitForElm('.filter');
-    console.log('Hello from timesheet improver!!!');
-    const addGitHubButton = async (filter: Element): Promise<void> => {
-        console.log('addGitHubButton');
-        const gitButton = filter.appendChild(document.createElement('button'));
-        console.log('gitButton:', gitButton);
-        gitButton.innerHTML = '<span class="buttonText">GitHub Import</span>';
-        gitButton.classList.add('protectButton');
-        gitButton.setAttribute(
-            'style',
-            'border: 0; font-family: "Open sans",Helvetica,Arial,sans-serif!important; background-color: inherit; font-weight: 500;'
-        )
-        gitButton.onclick = () => {
-            console.log('i clicked the Button!!');
-        };
-    };
+(() => {
+    waitForElm('.filter').then((filter: Element | null) => {
+        const addGitHubButton = async (filter: Element): Promise<void> => {
+            if (!filter) return;
 
-    if (filter) {
+            const gitButton = filter.appendChild(document.createElement('button'));
+            gitButton.innerHTML = '<span class="buttonText">GitHub Import</span>';
+            gitButton.classList.add('protectButton');
+            gitButton.setAttribute(
+                'style',
+                'border: 0; font-family: "Open sans",Helvetica,Arial,sans-serif!important; background-color: inherit; font-weight: 500;'
+            );
+            gitButton.onclick = () => {
+                console.log('i clicked the Button!!');
+            };
+        };
         addGitHubButton(filter as Element);
-    }
+    });
+
+    waitForElm('.settingsDetails').then((settingsDetail: Element | null) => {
+        if (!settingsDetail) return;
+
+        const addGitHubCredentialSettings = async (settingsDetail: Element) => {
+            const newSettingsDetailSection = settingsDetail.appendChild(
+                document.createElement('div')
+            );
+            newSettingsDetailSection.classList.add('msgDetailBlock');
+            newSettingsDetailSection.setAttribute('style', 'height: 400px; width: 100vw');
+
+            newSettingsDetailSection.innerHTML =
+                '<span class="buttonText">testtestests</span>';
+        };
+        addGitHubCredentialSettings(settingsDetail as Element);
+    });
 })();
