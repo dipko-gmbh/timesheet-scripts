f9d33dc967c2df2e343a138f1e8868fe1ae0f181
Markus Heine <markusheine.dev@gmail.com>
2022-10-21 13:27:58 +0200

    add button to dom

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('hello world wass:')
+import waitForElm from './helper/waitForElement';
+
+(async () => {
+    const filter = await waitForElm('.filter');
+    console.log('Hello from timesheet improver!!!');
+    const addGitHubButton = async (filter: Element): Promise<void> => {
+        console.log('addGitHubButton');
+        const gitButton = filter.appendChild(document.createElement('button'));
+        console.log('gitButton:', gitButton);
+        gitButton.innerHTML = '<span class="buttonText">GitHub Import</span>';
+        gitButton.classList.add('protectButton');
+        gitButton.setAttribute(
+            'style',
+            'border: 0; font-family: "Open sans",Helvetica,Arial,sans-serif!important; background-color: inherit; font-weight: 500;'
+        )
+        gitButton.onclick = () => {
+            console.log('i clicked the Button!!');
+        };
+    };
+
+    if (filter) {
+        addGitHubButton(filter as Element);
+    }
+})();
