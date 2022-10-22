efee49d96e1f71f1896ca8dee6df67768c838676
Birk Brauer <3842623+bb1@users.noreply.github.com>
2022-10-22 09:56:01 +0200

    added orga

--- src/index.ts
+++ src/index.ts
@@ -15,16 +15,18 @@ import waitForElm from './helper/waitForElement';
             gitButton.onclick = async () => {
                 const user = localStorage.getItem('github_user');
                 const pat = localStorage.getItem('github_pat');
-                if (!user || !pat) {
-                    console.log('no GitHub user or pat');
+                const orga = localStorage.getItem('github_orga');
+
+                if (!user || !pat || !orga) {
+                    console.log('no GitHub user or pat or orga');
                     return;
                 }
                 // get REPOS
-                const url = `https://api.github.com/orgs/dipko-gmbh/repos?affiliation=owner&per_page=100`; // visibility=public
+                const url = `https://api.github.com/orgs/${orga}/repos?affiliation=owner&per_page=100`; // visibility=public
                 const repos = await fetch(url, {
                     headers: {
                         Authorization: `Basic ${btoa(`${user}:${pat}`)}`,
-                        // Authorization: `token ${pat}}`,
+                        // Authorization: `Bearer ${pat}}`,
                         'Content-Type': 'application/json',
                         Accept: 'application/vnd.github.v3+json',
                     },
@@ -39,15 +41,19 @@ import waitForElm from './helper/waitForElement';
         if (!settingsDetail) return;
 
         const addGitHubCredentialSettings = async (settingsDetail: Element) => {
-            const user = localStorage.getItem('github_user');
-            const pat = localStorage.getItem('github_pat');
+            const user = localStorage.getItem('github_user') || '';
+            const pat = localStorage.getItem('github_pat') || '';
+            const orga = localStorage.getItem('github_orga') || '';
 
             const newSettingsDetailSection = settingsDetail.appendChild(document.createElement('div'));
             newSettingsDetailSection.classList.add('msgDetailBlock', 'github_connection');
-            // newSettingsDetailSection.setAttribute('style', 'width: 100vw');
 
             newSettingsDetailSection.innerHTML =
                 '<div class="header">GitHub Credentials</div>' +
+                '<div class="grid">' +
+                    '<div class="label">Orga</div>' +
+                    `<div class="value"><input class="msgInput" id="github_orga" value="${orga}"/></div>` +
+                '</div>' + 
                 '<div class="grid">' +
                     '<div class="label">Username</div>' +
                     `<div class="value"><input class="msgInput" id="github_user" value="${user}"/></div>` +
@@ -59,10 +65,9 @@ import waitForElm from './helper/waitForElement';
 
             newSettingsDetailSection.querySelector('#github_user')?.addEventListener('change', setLocalStorageFromInput);
             newSettingsDetailSection.querySelector('#github_pat')?.addEventListener('change', setLocalStorageFromInput);
+            newSettingsDetailSection.querySelector('#github_orga')?.addEventListener('change', setLocalStorageFromInput);
         };
         addGitHubCredentialSettings(settingsDetail);
-
-        waitForElm('.github_connection')
     });
 })();
 
