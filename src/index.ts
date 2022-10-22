90e4cf20cc27c5e9d44c5b07f43ef32169b75643
Birk Brauer <3842623+bb1@users.noreply.github.com>
2022-10-22 10:27:35 +0200

    fetch github extracted + removed orga again

--- src/index.ts
+++ src/index.ts
@@ -15,23 +15,35 @@ import waitForElm from './helper/waitForElement';
             gitButton.onclick = async () => {
                 const user = localStorage.getItem('github_user');
                 const pat = localStorage.getItem('github_pat');
-                const orga = localStorage.getItem('github_orga');
 
-                if (!user || !pat || !orga) {
+                if (!user || !pat) {
                     console.log('no GitHub user or pat or orga');
                     return;
                 }
-                // get REPOS
-                const url = `https://api.github.com/orgs/${orga}/repos?affiliation=owner&per_page=100`; // visibility=public
-                const repos = await fetch(url, {
-                    headers: {
-                        Authorization: `Basic ${btoa(`${user}:${pat}`)}`,
-                        // Authorization: `Bearer ${pat}}`,
-                        'Content-Type': 'application/json',
-                        Accept: 'application/vnd.github.v3+json',
-                    },
-                }).then((res) => res.json());
-                console.log(repos);
+
+                // get repos from GitHub
+                const path = `user/repos`;
+                const allRepos = (await fetchGithubApi(path)).map((repo: {full_name: string}) => repo.full_name) as string[];
+                const relevantRepos = allRepos.filter((repo) => !repo.startsWith(user));
+
+                console.log(relevantRepos);
+
+                const dateStart = new Date();
+                dateStart.setHours(0, 0, 0, 0);
+                const dateEnd = new Date();
+                dateEnd.setHours(23, 59, 59, 999);
+
+                // get commits from GitHub
+                const commits = (await Promise.all(
+                    relevantRepos.map((repo) => {
+                        const url = `repos/${repo}/commits?per_page=100&author=${user}` +
+                            `&since=${dateStart.toISOString()}&until=${dateEnd.toISOString()}`;
+                        const commits = fetchGithubApi(url);
+                        return commits;
+                    })
+                )).flat()
+
+                console.log(commits);
             };
         };
         addGitHubButton(filter);
@@ -43,17 +55,12 @@ import waitForElm from './helper/waitForElement';
         const addGitHubCredentialSettings = async (settingsDetail: Element) => {
             const user = localStorage.getItem('github_user') || '';
             const pat = localStorage.getItem('github_pat') || '';
-            const orga = localStorage.getItem('github_orga') || '';
 
             const newSettingsDetailSection = settingsDetail.appendChild(document.createElement('div'));
             newSettingsDetailSection.classList.add('msgDetailBlock', 'github_connection');
 
             newSettingsDetailSection.innerHTML =
                 '<div class="header">GitHub Credentials</div>' +
-                '<div class="grid">' +
-                    '<div class="label">Orga</div>' +
-                    `<div class="value"><input class="msgInput" id="github_orga" value="${orga}"/></div>` +
-                '</div>' + 
                 '<div class="grid">' +
                     '<div class="label">Username</div>' +
                     `<div class="value"><input class="msgInput" id="github_user" value="${user}"/></div>` +
@@ -65,11 +72,24 @@ import waitForElm from './helper/waitForElement';
 
             newSettingsDetailSection.querySelector('#github_user')?.addEventListener('change', setLocalStorageFromInput);
             newSettingsDetailSection.querySelector('#github_pat')?.addEventListener('change', setLocalStorageFromInput);
-            newSettingsDetailSection.querySelector('#github_orga')?.addEventListener('change', setLocalStorageFromInput);
         };
         addGitHubCredentialSettings(settingsDetail);
     });
 })();
+
+function fetchGithubApi(path: string) {
+    const user = localStorage.getItem('github_user');
+    const pat = localStorage.getItem('github_pat');
+
+    return fetch('https://api.github.com/' + path, {
+        headers: {
+            Authorization: `Basic ${btoa(`${user}:${pat}`)}`,
+            // Authorization: `Bearer ${pat}}`,
+            'Content-Type': 'application/json',
+            Accept: 'application/vnd.github.v3+json',
+        },
+    }).then((res) => res.json());
+}
 
 function setLocalStorageFromInput(event: Event): void {
     const target = event.target as HTMLInputElement;
